04df09de06c5cbe2ea90d5e6850d1607400e8af5
tobe <tobegit3hub@gmail.com>
2014-10-23 17:12:35 +0800

    Add Godep dependent files and run godep save

---
+++ Godeps/_workspace/src/github.com/astaxie/beego/example/chat/controllers/default.go
@@ -0,0 +1,20 @@
+// Beego (http://beego.me/)
+// @description beego is an open-source, high-performance web framework for the Go programming language.
+// @link        http://github.com/astaxie/beego for the canonical source repository
+// @license     http://github.com/astaxie/beego/blob/master/LICENSE
+// @authors     Unknwon
+
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type MainController struct {
+	beego.Controller
+}
+
+func (this *MainController) Get() {
+	this.Data["host"] = this.Ctx.Request.Host
+	this.TplNames = "index.tpl"
+}
